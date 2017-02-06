e2000184146c09a698820d456f51f88c4b8e4af4
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-02-06 17:52:39 -0500

    worked on js.victims

---
+++ js.victims/victims.js
@@ -0,0 +1,14 @@
+var victims = prompt ("How many disaster victims would you like to enter?") 
+ 
+for (var i = victims; i > 0; i--) {
+  if (victims > 0) {
+    var name = prompt ("What is the victim's name?");
+    var number = prompt("What is " + name + "'s number?");
+    var street = prompt("What is " + name + "'s street?");
+ } 
+}
+
+//alert (name+'\n'+number+'\n'+street);
+  
+
+
