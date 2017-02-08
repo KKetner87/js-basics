acb55c688d211fd1fdbaaac228cb5149c93fe9f9
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-02-08 18:04:52 -0500

    figuring out js.victims using maps

--- js.victims/victims.js
+++ js.victims/victims.js
@@ -1,16 +1,27 @@
-var victims = prompt ("How many disaster victims would you like to enter?") 
- 
-//* for loop
+//1. First we need to use a prompt to ask the user how many victims to enter.
+// 2. Next, we need to ask for the victim's info: name, number, and street.
+//    a. store this info in 3 seperate arrays, one for each seperate piece of info, using the .push method.
+//3. Then we need to continue this loop for the number of victims entered.
+// 4. We need to repeat all the above steps and include the data in a seperate array for volenteers.
+//5. At the end, print out  in a single alert, the number of victims, the number of volunteers, and a list of all victims and all volunteers.
 
+var vic
+
+
+var vicPrompt = prompt ("How many disaster victims do you wish to enter?");
+var vicName = prompt ("What is the victim's name?");
+var vicNumber = prompt("What is " + name + "'s number?");
+var vicStreet = prompt("What is " + name + "'s street?");
+
+
+
+var vicInfo = [];
 for (var i = victims; i > 0; i--) {
-  if (victims > 0) {
-    var name = prompt ("What is the victim's name?");
-    var number = prompt("What is " + name + "'s number?");
-    var street = prompt("What is " + name + "'s street?");
+  vicInfo.push ()
+
  } 
 }
 
-alert ([name(victims.length)+'\n'+number(victims.length)+'\n'+street(victims.length)]);
-
+//alert ([name(victims.length)+'\n'+number(victims.length)+'\n'+street(victims.length)]);
 
 //* map method with for..of loop 
