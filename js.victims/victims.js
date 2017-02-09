d9f685773a2a37c62f8ead266104598b38c90ed7
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-02-09 14:36:37 -0500

    Finished the js.victims

--- js.victims/victims.js
+++ js.victims/victims.js
@@ -1,27 +1,43 @@
-//1. First we need to use a prompt to ask the user how many victims to enter.
+// 1. First we need to use a prompt to ask the user how many victims to enter.
 // 2. Next, we need to ask for the victim's info: name, number, and street.
-//    a. store this info in 3 seperate arrays, one for each seperate piece of info, using the .push method.
-//3. Then we need to continue this loop for the number of victims entered.
+//    a. = store this info in 3 seperate arrays, one for each seperate piece of info, using the .push method.
+// 3. Then we need to continue this loop for the number of victims entered.
 // 4. We need to repeat all the above steps and include the data in a seperate array for volenteers.
-//5. At the end, print out  in a single alert, the number of victims, the number of volunteers, and a list of all victims and all volunteers.
-
-var vic
+// 5. At the end, print out  in a single alert, the number of victims, the number of volunteers, and a list of all victims and all volunteers.
 
 
-var vicPrompt = prompt ("How many disaster victims do you wish to enter?");
-var vicName = prompt ("What is the victim's name?");
-var vicNumber = prompt("What is " + name + "'s number?");
-var vicStreet = prompt("What is " + name + "'s street?");
+var victims = prompt ("How many disaster victims do you wish to enter?");
 
+var vicName =[];
+var vicNumber=[];
+var vicStreet=[];
 
-
-var vicInfo = [];
 for (var i = victims; i > 0; i--) {
-  vicInfo.push ()
+  if (victims > 0) {
+    vicName.push(prompt ("What is the victim's name?"));
+    vicNumber.push(prompt("What is the victim's number?"));
+    vicStreet.push(prompt("What is the victim's street?"));
+  }}
 
- } 
-}
 
-//alert ([name(victims.length)+'\n'+number(victims.length)+'\n'+street(victims.length)]);
+var volunteers = prompt ("How many disaster volunteers do you wish to enter?");
 
-//* map method with for..of loop 
+var volName =[];
+var volNumber=[];
+var volStreet=[];
+
+for (var i = volunteers; i > 0; i--) {
+  if (volunteers > 0) {
+    volName.push(prompt ("What is the volunteer's name?"));
+    volNumber.push(prompt("What is the volunteer's number?"));
+    volStreet.push(prompt("What is the volunteer's street?"));
+  }}
+
+
+
+alert ("There are " + victims + " victims and " + volunteers + " volunteers. The victims' names are " + vicName.join (", ") + ".\n The volunteers' names are " + volName.join (", ") + ".");
+
+
+
+
+
