6630ae68290e045b0876b2939ba3985a96488ccc
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-02-14 01:49:41 -0500

    finished the ng.intro!

--- ng.intro/part2.js
+++ ng.intro/part2.js
@@ -8,16 +8,30 @@ function transform () {
   var changes = this;
   console.log ("I'm working!");
   
-  //changes.changeColor = function(){
-    //parColor = {color: pink};
-  //}
-  
+
+
+// app.directive('confirmationNeeded', function () {
+//   return {
+//     priority: 1,
+//     terminal: true,
+//     link: function (scope, element, attr) {
+//       var msg = attr.confirmationNeeded || "Are you sure?";
+//       var clickAction = attr.ngClick;
+//       element.bind('click',function () {
+//         if ( window.confirm(msg) ) {
+//           scope.$eval(clickAction)
+//         }
+//       });
+//     }
+//   };
+// });
+
+
+    changes.leavePage = function() {
+        var answer = confirm("Are you sure you want to leave this page?")
+        if (!answer) {
+            event.preventDefault();
+        }
+    }
 }
 
-
-
-//<li ng-repeat="i in persons" ng-style="personColour" ng-mouseenter="changeColor(i)"></li>
-//$scope.changeColor = function(person) {
-//     /$scope.personColour = {color: '#'+person.colour};
-//};
-// 
