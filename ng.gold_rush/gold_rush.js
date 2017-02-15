ae6652491d37593111eddf2f50452315a3a653b1
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-02-14 19:17:02 -0500

    just getting started on gold rush

---
+++ ng.gold_rush/gold_rush.js
@@ -0,0 +1,25 @@
+angular.module("gold_rush",[]);
+
+angular
+      .module("gold_rush")
+      .controller("gold", goldCtrl);
+
+goldCtrl.$inject =[];
+
+function goldCtrl () {
+  var golden =this;
+  console.log ("it works!");
+  golden.addOnClick = function(event) {
+        golden.items.push( {
+            "label": "Click",
+            "value": 100,
+            "x": event.offsetX-50,
+            "y": event.offsety-50,
+        }) 
+} 
+  golden.checkClick = function(){
+    console.log(event);
+  }
+}
+
+
