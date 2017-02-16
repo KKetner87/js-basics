722fa019a9b47344a75d228586812a53860d3c94
Brian Hays <brianhays@Brians-MacBook-Pro.local>
2017-02-16 08:41:31 -0700

    The potatoe sticks on-click

--- ng.gold_rush/gold_rush.js
+++ ng.gold_rush/gold_rush.js
@@ -9,17 +9,31 @@ goldCtrl.$inject =[];
 function goldCtrl () {
   var golden =this;
   console.log ("it works!");
-  golden.addOnClick = function(event) {
-        golden.items.push( {
-            "label": "Click",
-            "value": 100,
-            "x": event.offsetX-50,
-            "y": event.offsety-50,
-        }) 
-} 
-  golden.checkClick = function(){
+//   golden.addOnClick = function(event) {
+//         golden.items.push( {
+//             "label": "Click",
+//             "value": 100,
+//             "x": event.offsetX-50,
+//             "y": event.offsety-50,
+//         })
+// }
+
+
+  golden.img = "http://vignette2.wikia.nocookie.net/nexttopthingy/images/c/c2/Potato.png/revision/latest?cb=20140421121844"
+
+
+  golden.lastClickInfo = {},
+  golden.mouseClick = function(event){
     console.log(event);
+
+    golden.lastClickInfo.pageX = event.pageX;
+    golden.lastClickInfo.pageY = event.pageY;
   }
+  // golden.marker = [];
+  // golden.addMarker = function () {
+  //   golden.newMarker = {x,y}
+  //   golden.marker.push(golden.newMarker);
+  // }
+
+
 }
-
-
