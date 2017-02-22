ceacafe3ae07b2120d70cc4f8733364f90983d3e
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-02-21 20:12:02 -0500

    added welcome page content and petpage modules. Need to add checkout page or module and filter

--- midterm_project/jspages/midterm_project.controller.petpage.js
+++ midterm_project/jspages/midterm_project.controller.petpage.js
@@ -1,34 +1,55 @@
-angular.module ("pawsitive")
-       .controller("petPage", petCtrl)
+angular.module("pawsitive")
+  .controller("petPage", petCtrl)
 
-petCtrl.$inject =["adoptInfo"];
+petCtrl.$inject = ["adoptInfo"];
 
 function petCtrl(adoptInfo) {
   var animal = this;
-  
+
   animal.available = adoptInfo.pet;
 
-animal.buyPet = function(selectedPet){
-  if(selectedPet.qty > 0){
-    selectedPet.qty--
+  animal.buyPet = function(selectedPet, ind) {
+   
+    $("#myModal" + ind).modal('hide');
+    $("#purchase" + ind).modal('show');
+    
   }
+
+  animal.purchase = function(selectedPet) {
+    console.log(selectedPet)
+    $("#close1_open2").click(function() {
+     if (selectedPet.qty > 0) {
+        selectedPet.qty--
+      }
+});
+  }
+
+
+
+
+// home.editAdopt = function(selectedPet){
+//     console.log(selectedPet)
+//     $('#selectedPet').modal('show');
+//     home.availablePet = selectedPet;
+//   }
+
+
+animal.isAvailable = function(selectedPet) {
+  return selectedPet.qty > 0
 }
-  animal.isAvailable = function(selectedPet){
-    return selectedPet.qty > 0
-  }
-  
+
 }
 
 //  home.greeting = "Our Selection : ";
-  
+
 //   home.inventory = StockRoom.stock;
-  
+
 //   home.buyBooze = function(boozeItem){
 //     if(boozeItem.qty > 0){
 //       boozeItem.qty--
 //     }
 //   }
-  
+
 //   home.isAvailable = function(boozeItem){
 //     return boozeItem.qty > 0
 //   }
