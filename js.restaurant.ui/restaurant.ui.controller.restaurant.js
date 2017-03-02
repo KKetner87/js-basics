0841bba2dad9e82b2c0eb701473f92593f1837d0
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-03-01 20:17:10 -0500

    need to add dietary restrictions filter to website

---
+++ js.restaurant.ui/restaurant.ui.controller.restaurant.js
@@ -0,0 +1,29 @@
+angular.module("OpenSourceTacos")
+.controller('restaurant', restaurantCtrl);
+
+restaurantCtrl.$inject =["restaurantInfo"]
+
+function restaurantCtrl(restaurantInfo) {
+  var restaurant =this;
+  
+  restaurant.tacoStand = restaurantInfo.tacoStand;
+  console.log(restaurantInfo);
+  restaurant.order = [];
+  
+  restaurant.name = "Open Source World of Tacos";
+  
+  
+  
+  restaurant.buy = function (item) {
+    confirm ("Do you want to add this " + item.name + " to your basket?");
+    if(item) {
+      restaurant.order.push(item);
+      console.log(restaurant.order);
+      restaurant.total =0;
+      for (var i = 0; i < restaurant.order.length; i++){
+        restaurant.total += restaurant.order[i].price;
+      }
+    }
+  }
+  
+}
---
+++ js.restaurant.ui/restaurant.ui.module.js
@@ -0,0 +1,26 @@
+angular.module("OpenSourceTacos", []);
+//        .config(Router)
+
+// Router.$inject =["$routeProvider"];
+
+// function Router($routeProvider) {
+//   console.log("loading router")
+//   $routerProvider
+//       .when('/', {
+//       templateUrl: '/home.html',
+//       controller: 'restaurant as restaurant'
+//     })
+//     .when('/menu', {
+//       templateUrl: '/menu.html',
+//       controller: 'restaurant as restaurant'
+//     })
+//       .when('/order', {
+//       templateUrl: '/order.html',
+//       controller: 'order as order'
+//     })
+//     .otherwise({
+//       redirect: '/'
+//     });
+//  }
+
+
