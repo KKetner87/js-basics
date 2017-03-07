e6afa423188f7e2386bb7413603a423c8b8f3925
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-03-07 14:48:07 -0500

    finished loop practice 2

---
+++ js.loop_practice2/loop_practice2.js
@@ -0,0 +1,46 @@
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+
+
+// problem one: rat cat butterfly marmot
+
+// for(var i=0; i < animals.length; i++){
+//   console.log(animals[i-1]);
+// }
+
+
+
+
+
+// problem two: rat butterfly ocelot
+
+// for(var i=0; i < animals.length; i++){
+//   console.log(animals.splice(i, 1));
+// }
+
+
+
+
+
+// problem three: ocelot marmot butterfly cat rat
+
+// animals.reverse();
+// console.log(animals);
+
+
+
+
+
+// problem four: rat cat cat butterfly butterfly marmot marmot ocelot
+
+// animals.splice(2, 0, 'cat', 'butterfly');
+// animals.splice(6, 0, 'marmot');
+
+// console.log(animals); 
+
+
+
+
+
+
+
