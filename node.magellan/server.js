e95a1f113f92379956137eec5805d0fc969adc40
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-03-07 18:30:08 -0500

    finished magellan's voyage

--- node.magellan/server.js
+++ node.magellan/server.js
@@ -1,13 +1,67 @@
-var express = require('express'),
-  PORT = 3000,
-  app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Seville, where Magellan departed Spain in 1519. Next he sailed to Canary Islands!</h1><form method="post" action="/submit"><input type = submit> Go to Canary Islands!</form> ")
+var express=require("express");
+   bodyParser = require("body-parser");
+   app= express(),
+   PORT=3000;
+
+app.use(
+bodyParser.json(), 
+bodyParser.urlencoded ({extended : true})
+)
+
+
+   
+app.get('/',(req,res)=>{
+  console.log(req.query);
+
+res.sendFile("seville.html", {root : './'});
+
+ 
+ });
+
+ app.get('/Canary',(req,res)=>{
+ 
+res.sendFile("canary.html",{root:'./'});
+ 
 });
 
-app.post('/click', (res, req) =>{
-  res.redirect('/canaryislands');
+
+app.get('/Cape',(req,res)=>{
+ 
+res.sendFile("cape.html",{root:'./'});
+ 
+});
+
+app.get('/Strait',(req,res)=>{
+ 
+res.sendFile("strait.html",{root:'./'});
+ 
+});
+
+app.get('/Guam',(req,res)=>{
+ 
+res.sendFile("guam.html",{root:'./'});
+ 
+});
+
+app.get('/Philippines',(req,res)=>{
+
+ res.sendFile("philippines.html",{root:'./'});
+  
+});
+
+
+//bonus: a /next route which will serve up a JSON response //
+
+app.get('/next', (req, res) =>{
+  
+  res.redirect('/' + req.query.location);
+  console.log(req.query);
+})
+
+//bonus: Add a route to handle an unknown destination. //
+app.get('/:places', (req, res)=>{
+  res.send(`Sorry! Magellan never traveled to ${req.params.places}! He missed out!`)
 });
 
 
@@ -17,6 +71,11 @@ app.post('/click', (res, req) =>{
 
 
 
-app.listen(PORT, (res) => {
-  console.log('server is on port 3000!');
+
+
+
+app.listen(PORT,()=>{
+ 
+ console.log("Server is working");
+
 });
