4e24ffeda284523b429f9187e2a9714877a02549
runningwithscissors09 <runningwithscissors09@gmail.com>
2017-03-14 11:11:40 -0400

    finish second problem

--- js.problem_set4/problem_set4.js
+++ js.problem_set4/problem_set4.js
@@ -4,21 +4,70 @@
 // and "5Hello 5" should return two different answers. Each string will contain at least one letter or symbol. 
 // Warning: Regex is not allowed for this exercise!
 
-function addNumbers (string) {
-  for(var i = 0; i < string.length; i++){
-    if(i !== Nan){
-    } if ((i+1) !== Nan) {
-      
-    }
-  }
+
+
+//failed attempt//
+
+// function addNumbers(string) {
+  
+//   var Numbers =[];
+  
+  
+//   for (var i = 0; i < string.length; i++) {
+//     for (var a = 0; a < string.length; a++) {
+//       if (typeof i === 'number' && typeof(i + 1) === 'number') {
+//         firstNum = parseInt(i + (i + 1));
+//       }
+//       if (typeof a === 'number') {
+//         secondNum = parseInt(a);
+//       }
+//     }
+//     return(firstNum + secondNum);
+// } 
+// }
+
+
+function number(num){
+ // return !isNaN(parseFloat(num)) && isFinite(num);
+
+ if (typeof (parseInt(num)) === 'number' && isNaN(parseInt(num)) === false && isFinite(parseInt(num)) === true){
+   return num
+ }
 }
 
-//isFinite
-//typeof
+function addNumbers(string){
+
+var numbers = []
+
+ for (var i = 0; i < string.length; i++){
+
+   if(number(string[i]) == string[i]){
+     numbers.push(string[i])
+
+     var x = i+1;
+
+     while(string[x] == number(string[x])&& x <= string.length){
+       numbers[numbers.length-1] += string[x];
+       // console.log(numbers)
+       x++;
+     }
+     i = x;
+     // numbers.push(string[i])
+   }
+
+ }for (var a = 0 ; a < numbers.length ; a ++){
+   return numbers
+         .map(function(str) { return parseInt(str); })
+         .reduce(function(x,y) {return x+y});
+
+   }
+}
+
+console.log(addNumbers("88Hello 3World"));
+console.log(addNumbers("55He55l8l5o"));
+console.log(addNumbers("5Hello 5"));
 
 
-
-addNumbers("88Hello 3World!");
 
 
 
@@ -26,7 +75,20 @@ addNumbers("88Hello 3World!");
 // Write a function longestWord that takes a single string parameter and returns the largest word in the string. 
 // If there are two or more words that are the same length, it returns the first word from the string with that length.
 // Ignore punctuation and assume the input sentence will not be empty.
+var long =[];
 
+function longestWord (string) {
+
+  for (var i = 0; i < string.length; i++) {
+    if(string[i].length > string[i+1].length){
+     long.push(string[i]);
+    } else {
+    long.push(string[i+1]);  
+  } 
+  }   
+}
+
+console.log(long['Hello my friend']);
 
 
 
