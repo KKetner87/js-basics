f149d2fa6b2eaf130657b11f33adfabc357ea260
Brian Hays <brianhays@Brians-MacBook-Pro.local>
2017-03-16 09:08:23 -0600

    says hello

--- node.lingo/public/js/translate_controller.js
+++ node.lingo/public/js/translate_controller.js
@@ -1,27 +1,52 @@
 angular.module("lingoMod")
        .controller("lingoCtrl", lingoCtrl);
 
-function lingoCtrl () {
+lingoCtrl.$inject = ['$http'];
+
+
+function lingoCtrl ($http) {
   var lingo = this;
-  
+
   lingo.greeting="Welcome to Say 'Hello!'"
-  
-  
-  function translate($http){
-    console.log('something')
-    ({method:'POST',
+
+
+  lingo.translate = function (){
+
+    $http({method:'POST',
     url: '/translate',
       data : {
-        word:lingo.word,
+        word:'hello',
         from: 'en',
         to: lingo.lang
       }
   }).then(function(res){
-      res.data
+      console.log(res.data);
+
+      lingo.resData = res.data.translatedText;
+      console.log(lingo.resData)
     })
   }
-    
-  
-  
-  
+
 }
+
+
+// mCtrl.page = function () {
+//
+//     // simulate a twilio POST request
+//     // https://www.twilio.com/console/phone-numbers/incoming
+//     var page = {
+//         To: 'TwilioPhone',  // twilio number
+//         From: 'BRETSA',     // BRETSA account, Boulder Regional Emergency Telephone Service Authority (BRETSA)
+//         Body: mCtrl.textMsg
+//     }
+//
+//     console.log("POST: /page", page);
+//     $http.post('/page', page).then(
+//         function success(response) {
+//             console.log("POST:/page succeeded");
+//         },
+//         function failure(response) {
+//             console.log("POST:/page error:", response);
+//         }
+//     )
+// }
