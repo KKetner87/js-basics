8b6376f75fa42375836864d500832b7239a8ef61
Brian Hays <brianhays@Brians-MacBook-Pro.local>
2017-03-17 14:35:16 -0600

    more work with the quiz

--- node.lingo/public/js/quiz_controller.js
+++ node.lingo/public/js/quiz_controller.js
@@ -7,38 +7,50 @@ quizCtrl.$inject = ['$http'];
 function quizCtrl ($http) {
   var quiz = this;
 
-
+  quiz.count = 0;
+  quiz.fail=false;
+  quiz.correctAnswers = false;
 
   quiz.question = {
     words :[{word : 'one',
            answer : '',
+           correctAnswer: '',
            correct : ''},
           { word : 'two',
            answer : '',
+           correctAnswer: '',
            correct : ''},
            {word : 'three',
            answer : '',
+           correctAnswer: '',
            correct : ''},
           { word : 'four',
            answer : '',
+           correctAnswer: '',
            correct : ''},
            {word : 'five',
            answer : '',
+           correctAnswer: '',
            correct : ''},
           { word : 'six',
            answer : '',
+           correctAnswer: '',
            correct : ''},
            {word : 'seven',
            answer : '',
+           correctAnswer: '',
            correct : ''},
           { word : 'eight',
            answer : '',
+           correctAnswer: '',
            correct : ''},
            {word : 'nine',
            answer : '',
+           correctAnswer: '',
            correct : ''},
           { word : 'ten',
            answer : '',
+           correctAnswer: '',
            correct : ''},
 
   ]}
@@ -57,8 +69,10 @@ function quizCtrl ($http) {
       }
   }).then(function(res){
       quiz.resData = res.data;
+
       quiz.quizLength = quiz.resData.length;
-      console.log(quiz.quizLength)
+      // console.log(quiz.quizLength)
+      quiz.correctAnswers = true;
 
 
       ////////////////////////////////////////////
@@ -68,18 +82,33 @@ function quizCtrl ($http) {
       for (var i = 0; i < quiz.quizLength; i++){
         quiz.correct = false;
         quiz.Incorrect = false;
-        console.log('Correct Answer ' + quiz.resData[i].translatedText.toLowerCase() + ' Your Answer ' + quiz.question.words[i].answer)
+        // console.log('Correct Answer ' + quiz.resData[i].translatedText.toLowerCase() + ' Your Answer ' + quiz.question.words[i].answer)
         if (quiz.resData[i].translatedText.toLowerCase() == quiz.question.words[i].answer.toLowerCase()){
-          console.log('Awesome')
+          // console.log('Awesome')
           quiz.question.words[i].correct = true;
+          // quiz.question.words[i].correctAnswer = res.data[i].translatedText
         }else{
-          console.log('No Dice')
+          // console.log('No Dice')
           quiz.question.words[i].correct = false;
-        }
+          quiz.question.words[i].correctAnswer = res.data[i].translatedText;
+          quiz.count += 1
+          quiz.question.words[i].answer = '';
+
+
+            // quiz.fail=true;
+          }
+
 
         // console.log(quiz.resData[i].translatedText, quiz.question.words[i].word, quiz.question.words[i].answer)
       }
+      // console.log(quiz.count)
 
+      if(quiz.count > 2){
+        alert('You Failed.');
+      //   quiz.question.words[i].answer = '';
+
+        // quiz.fail=true;
+      }
 
 
 
